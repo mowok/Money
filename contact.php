b46575fde657b475133b3338a8524dda3f89592d
mowok <mowok21@gmail.com>
2017-05-07 17:14:53 +0300

    v.2

--- contact.php
+++ contact.php
@@ -3,5 +3,5 @@ require 'Init.php';
 $grid = $app->layout->add('CRUD');
 $grid->setModel(new friends($db));
 $grid->addQuickSearch();
-
+$grid->addAction('Loan',new \atk4\ui\jsExpression('document.location="lend.php?user_id="+$(this).closest("tr").data("id");'));
 ?>
